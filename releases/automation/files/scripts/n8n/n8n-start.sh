c5ab0f631d86f282a947bfd808d9eb0ecda2fb77
Alvaro Santos Andres <alvsanand@gmail.com>
2025-05-19 19:43:17 +0200

    feat: enable external function support in n8n node (6)

--- releases/automation/files/scripts/n8n/n8n-start.sh
+++ releases/automation/files/scripts/n8n/n8n-start.sh
@@ -3,22 +3,23 @@
 N8N_NODE_MODULES_PATH=/usr/local/lib/node_modules/n8n/node_modules
 N8N_COMUNITY_NODES_PATH=$HOME/.n8n/nodes
 
-if [ -n "$CUSTOM_EXTERNAL_MODULES" ]; then
-  for lib in $(echo "$CUSTOM_EXTERNAL_MODULES" | sed "s/,/ /g"); do
-    echo "Installing external library: $lib"
+if [ -n "$CUSTOM_EXTRA_MODULES" ]; then
+  for lib in $(echo "$CUSTOM_EXTRA_MODULES" | sed "s/,/ /g"); do
+    echo "Installing extra library: $lib"
 
-    npm i --prefix "$N8N_NODE_MODULES_PATH" "$lib" || echo "Error Installing external library: $lib"
+    npm i --prefix "$N8N_NODE_MODULES_PATH" "$lib" || echo "Error Installing extra library: $lib"
   done
 fi
 
 if [ -n "$CUSTOM_COMMUNITY_NODES" ]; then
-  mkdir -p "$N8N_COMUNITY_NODES_PATH"
   
-
   for lib in $(echo "$CUSTOM_COMMUNITY_NODES" | sed "s/,/ /g"); do
-    echo "Installing external library: $lib"
+    COMUNITY_NODES_PATH="$N8N_COMUNITY_NODES_PATH/node_modules/$lib"
+    mkdir -p "$COMUNITY_NODES_PATH"
 
-    npm i --prefix "$N8N_COMUNITY_NODES_PATH" "$lib" || echo "Error Installing community node: $lib"
+    echo "Installing community node: $lib"
+
+    npm i --prefix "$COMUNITY_NODES_PATH" "$lib" || echo "Error Installing community node: $lib"
   done
 fi
 
