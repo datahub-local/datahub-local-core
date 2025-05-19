b57ba43681c65983e18b7ca32d6d3b45364eba29
Alvaro Santos Andres <alvsanand@gmail.com>
2025-05-19 17:11:40 +0200

    feat: enable external function support in n8n node (4)

--- releases/automation/files/scripts/n8n/n8n-start.sh
+++ releases/automation/files/scripts/n8n/n8n-start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if [ -n "$NODE_FUNCTION_ALLOW_EXTERNAL" ]; then
-  for lib in $(echo "$NODE_FUNCTION_ALLOW_EXTERNAL" | sed "s/,/ /g"); do
+N8N_NODE_MODULES_PATH=/usr/local/lib/node_modules/n8n/node_modules
+
+if [ -n "$CUSTOM_EXTERNAL_MODULES" ]; then
+  for lib in $(echo "$CUSTOM_EXTERNAL_MODULES" | sed "s/,/ /g"); do
     echo "Installing external library: $lib"
 
-    npm i "$lib" || echo "Error Installing external library: $lib"
+    npm i --prefix "$N8N_NODE_MODULES_PATH" "$lib" || echo "Error Installing external library: $lib"
   done
 fi
 
