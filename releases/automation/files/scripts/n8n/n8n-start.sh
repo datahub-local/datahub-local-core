4e2e23e595ac8afc8aff1d454c1bd9c06f62a03c
Alvaro Santos Andres <alvsanand@gmail.com>
2025-05-20 11:48:23 +0200

    feat: enable external function support in n8n node (14)

--- releases/automation/files/scripts/n8n/n8n-start.sh
+++ releases/automation/files/scripts/n8n/n8n-start.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
-N8N_NODE_MODULES_PATH=/usr/local/lib/node_modules/n8n/node_modules/
-N8N_COMMUNITY_NODES_PATH=$HOME/node_modules
+N8N_NODE_MODULES_PATH=/usr/local/lib/node_modules/n8n
+NODE_USER=node
 
 if [ -n "$CUSTOM_EXTRA_MODULES" ]; then
   cd $N8N_NODE_MODULES_PATH
-
   for lib in $(echo "$CUSTOM_EXTRA_MODULES" | sed "s/,/ /g"); do
     echo "Installing extra library: $lib"
 
-    npm i "$lib" || echo "Error Installing extra library: $lib"
+    npm add "$lib" || echo "Error Installing extra library: $lib"
   done
 fi
 
-if [ -n "$CUSTOM_COMMUNITY_NODES" ]; then
-  cd $N8N_COMMUNITY_NODES_PATH
-  
+if [ -n "$CUSTOM_COMMUNITY_NODES" ]; then  
   for lib in $(echo "$CUSTOM_COMMUNITY_NODES" | sed "s/,/ /g"); do
     echo "Installing community node: $lib"
-
-    npm i "$lib" || echo "Error Installing community node: $lib"
+    su $NODE_USER -c "cd && npm i $lib" || echo "Error Installing community node: $lib"
   done
 fi
 
-su - node sh /docker-entrypoint.sh "$@"
+CMD="sh /docker-entrypoint.sh $@"
+su $NODE_USER -c "$CMD"
