9705304d7c798283ed645f76f180972ec4f2e6c2
Alvaro Santos Andres <alvsanand@gmail.com>
2025-07-13 08:41:28 +0200

    fix: add css kopia-nginx and fix n8n node dependencies install

--- releases/automation/files/scripts/n8n/n8n-start.sh
+++ releases/automation/files/scripts/n8n/n8n-start.sh
@@ -3,19 +3,21 @@
 N8N_NODE_MODULES_PATH=/usr/local/lib/node_modules/n8n
 NODE_USER=node
 
+npm -g install pnpm
+
 if [ -n "$CUSTOM_EXTRA_MODULES" ]; then
   cd $N8N_NODE_MODULES_PATH
   for lib in $(echo "$CUSTOM_EXTRA_MODULES" | sed "s/,/ /g"); do
     echo "Installing extra library: $lib"
 
-    npm add "$lib" || echo "Error Installing extra library: $lib"
+    pnpm add "$lib" || echo "Error Installing extra library: $lib"
   done
 fi
 
 if [ -n "$CUSTOM_COMMUNITY_NODES" ]; then  
   for lib in $(echo "$CUSTOM_COMMUNITY_NODES" | sed "s/,/ /g"); do
     echo "Installing community node: $lib"
-    su $NODE_USER -c "cd && npm i $lib" || echo "Error Installing community node: $lib"
+    su $NODE_USER -c "cd && pnpm install $lib" || echo "Error Installing community node: $lib"
   done
 fi
 
