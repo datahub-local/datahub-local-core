c667b5d27135141cedff0a41fd061292de06ba82
Alvaro Santos Andres <alvsanand@gmail.com>
2025-07-15 20:52:08 +0200

    revert: use of npm in n8n

--- releases/automation/files/scripts/n8n/n8n-start.sh
+++ releases/automation/files/scripts/n8n/n8n-start.sh
@@ -3,21 +3,19 @@
     N8N_NODE_MODULES_PATH=/usr/local/lib/node_modules/n8n
     NODE_USER=node
     
-    npm -g install pnpm
-    
     if [ -n "$CUSTOM_EXTRA_MODULES" ]; then
       cd $N8N_NODE_MODULES_PATH
     
       CUSTOM_EXTRA_MODULES=$(echo "$CUSTOM_EXTRA_MODULES" | sed "s/,/ /g")
       echo "Installing extra modules: $CUSTOM_EXTRA_MODULES"
     
-      pnpm add --shamefully-hoist --node-linker=hoisted --dangerously-allow-all-builds "$CUSTOM_EXTRA_MODULES" || echo "Error Installing extra modules: $CUSTOM_EXTRA_MODULES"
+      npm add "$CUSTOM_EXTRA_MODULES" || echo "Error Installing extra modules: $CUSTOM_EXTRA_MODULES"
     fi
     
     if [ -n "$CUSTOM_COMMUNITY_NODES" ]; then
       CUSTOM_COMMUNITY_NODES=$(echo "$CUSTOM_COMMUNITY_NODES" | sed "s/,/ /g")
       echo "Installing community nodes: $CUSTOM_COMMUNITY_NODES"
-      su $NODE_USER -c "cd && pnpm install --shamefully-hoist --node-linker=hoisted --dangerously-allow-all-builds $CUSTOM_COMMUNITY_NODES" || echo "Error Installing community nodes: $CUSTOM_COMMUNITY_NODES"
+      su $NODE_USER -c "cd && npm install $CUSTOM_COMMUNITY_NODES" || echo "Error Installing community nodes: $CUSTOM_COMMUNITY_NODES"
     fi
     
     CMD="sh /docker-entrypoint.sh $@"
